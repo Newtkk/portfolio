c38351b765c101bc857834f5fe30a567bc0dbcae
Newton Santos <158478870+Newtkk@users.noreply.github.com>
2024-04-16 21:24:57 -0300

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+$("#home-button").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#home").offset().top
+    }, 2000);
+})
+
+$("#habilidades-button").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#habilidades").offset().top
+    }, 2000);
+})
