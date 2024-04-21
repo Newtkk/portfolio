6de294db2f23db041ac00576a13db7c8d357dc13
Newtkk <ruandylon7@gmail.com>
2024-04-21 20:24:34 -0300

    responsividade ajustada

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,14 @@
-let icone = document.querySelectorAll('.icon')
-let corpo = document.querySelector('body')
+let icone = document.querySelectorAll('.icon');
+let corpo = document.querySelector('body');
 
 icone[1].addEventListener('click', ()=> {
-    icone[1].classList.toggle('mode')
-    icone[0].classList.toggle('mode')
-    corpo.classList.toggle('mode')
+    icone[1].classList.toggle('mode');
+    icone[0].classList.toggle('mode');
+    corpo.classList.toggle('mode');
 })
 
 icone[0].addEventListener('click', ()=> {
-    icone[1].classList.toggle('mode')
-    icone[0].classList.toggle('mode')
-    corpo.classList.toggle('mode')
+    icone[1].classList.toggle('mode');
+    icone[0].classList.toggle('mode');
+    corpo.classList.toggle('mode');
 })
